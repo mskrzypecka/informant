46f2e5058ed64dbae87f536e71ebe2213e74f2d1
mskrzypecka <malgorzata.skrzypecka@gmail.com>
2019-06-06 01:18:11 +0200

    register module added

--- informant/views.py
+++ informant/views.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -69,3 +71,17 @@ def add_comment_to_post(request, pk):
     else:
         form = CommentForm()
     return render(request,'informant/add_comment_to_post.html', {'form': form})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('post_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
